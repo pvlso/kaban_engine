8b60b6c22873866fb1e90b6d0f84a97c109a8eb7
BabyKaban <paul92219@proton.me>
2025-04-07 22:38:42 -0700

    Fixing assets mode canvas rendering to use perspactive renderging to acount on the screen size

--- code/editor_assets_mode.cpp
+++ code/editor_assets_mode.cpp
@@ -1289,7 +1289,8 @@ UpdateAndRenderAssetsMode(editor_state *EditorState, transient_state *TranState,
     b32 Result = false;//CheckForMetaInput(EditorState, TranState, Input);
     if(!Result)
     {
-        Orthographic(RenderGroup, 1.0f);
+        f32 Ratio = (f32)Nk->BaseWidth/(f32)Nk->BaseWidth;
+        Perspective(RenderGroup, Ratio, 2.0f, 2.16f);
         Clear(RenderGroup, UI_COLOR_RGBA1_4D3020FF);
 
         object_transform Flat = DefaultFlatTransform();
